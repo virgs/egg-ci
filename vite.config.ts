9f6080e033292e00ea954c35fb07fc0f4435d848
Guilherme Moraes <guilherme.moraes@outlook.com>
2024-02-27 16:04:55 -0800

    Set up project

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+      }
+
+    },
+    outDir: 'docs',
+    assetsDir: '.'
+  },
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+})
